7bd0a38a8f9f04b4a1f50bc91b266eddc206c6ba
Junfeng Qiao <qiaojunfeng@outlook.com>
2020-07-21 13:31:30 +0200

    Add `OpengridCalculation` for `open_grid.x` code
    `open_grid.x` unfolds a scf/nscf calculation on an irreducible BZ to a
    full BZ. It rewrites the eigenvalues and recalculates the wavefunctions.

---
+++ aiida_quantumespresso/parsers/opengrid.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from aiida.common import NotExistent
+from aiida.orm import KpointsData
+
+from aiida_quantumespresso.parsers.parse_raw.base import parse_output_base
+from aiida_quantumespresso.parsers.base import Parser
+
+
+class OpengridParser(Parser):
+    """`Parser` implementation for the `OpengridCalculation` calculation job class."""
+
+    def parse(self, **kwargs):
+        """Parse the retrieved files of a completed `OpengridCalculation` into output nodes."""
+        try:
+            out_folder = self.retrieved
+        except NotExistent:
+            return self.exit(self.exit_codes.ERROR_NO_RETRIEVED_FOLDER)
+
+        try:
+            filename_stdout = self.node.get_option('output_filename')
+            with out_folder.open(filename_stdout, 'r') as f:
+                out_file = f.read()
+        except OSError:
+            return self.exit(self.exit_codes.ERROR_OUTPUT_STDOUT_READ)
+
+        parsed_data, logs = parse_output_base(out_file, codename='OPEN_GRID')
+        self.emit_logs(logs)
+
+        lines = out_file.split('\n')
+        for line in lines:
+            if 'incompatible FFT grid' in line:
+                return self.exit(self.exit_codes.ERROR_INCOMPATIBLE_FFT_GRID)
+
+        if 'ERROR_OUTPUT_STDOUT_INCOMPLETE' in logs.error:
+            return self.exit(self.exit_codes.ERROR_OUTPUT_STDOUT_INCOMPLETE)
+        elif logs.error:
+            return self.exit(self.exit_codes.ERROR_GENERIC_QE_ERROR)
+
+        try:
+            kpoints_mesh, kpoints = self.parse_kpoints(out_file)
+        except ValueError:
+            return self.exit(self.exit_codes.ERROR_OUTPUT_KPOINTS_MISMATCH)
+
+        # Output both the dimensions and the explict list of kpoints
+        self.out('kpoints_mesh', kpoints_mesh)
+        self.out('kpoints', kpoints)
+
+    @classmethod
+    def parse_kpoints(cls, out_file):
+        """Parse and output the dimensions and the explicit list of kpoints"""
+        lines = out_file.split('\n')
+
+        kpoints = []
+        weights = []
+        found_kpoints = False
+
+        for line in lines:
+            if 'EXX: q-point mesh:' in line:
+                kmesh = [int(i) for i in line.strip().split()[-3:]]
+            if 'List to be put in the .win file of wannier90' in line:
+                found_kpoints = True
+                continue
+            if found_kpoints:
+                line = line.strip()
+                if line != '':
+                    line = [float(i) for i in line.split()]
+                    kpoints.append(line[:-1])
+                    weights.append(line[-1])
+                else:
+                    found_kpoints = False
+
+        kpoints_mesh = KpointsData()
+        kpoints_mesh.set_kpoints_mesh(kmesh)
+        kpoints_list = KpointsData()
+        kpoints_list.set_kpoints(kpoints, cartesian=False, weights=weights)
+
+        if kmesh[0] * kmesh[1] * kmesh[2] != len(kpoints):
+            raise ValueError('Mismatch between kmesh dimensions and number of kpoints')
+
+        return kpoints_mesh, kpoints_list
---
+++ tests/parsers/test_opengrid.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
+"""Tests for the `OpengridParser`."""
+
+
+def test_opengrid_default(fixture_localhost, generate_calc_job_node, generate_parser, data_regression, num_regression):
+    """Test ``OpengridParser`` on the results of a simple ``open_grid.x`` calculation."""
+    entry_point_calc_job = 'quantumespresso.opengrid'
+    entry_point_parser = 'quantumespresso.opengrid'
+
+    node = generate_calc_job_node(entry_point_calc_job, fixture_localhost, 'default')
+    parser = generate_parser(entry_point_parser)
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+
+    for link_name in ['kpoints_mesh', 'kpoints']:
+        assert link_name in results, list(results.keys())
+
+    data_regression.check({
+        'kpoints_mesh': results['kpoints_mesh'].attributes,
+        'kpoints': results['kpoints'].attributes,
+    })
+
+    num_regression.check({
+        'kpoints_array0': results['kpoints'].get_array('kpoints')[:, 0],
+        'kpoints_array1': results['kpoints'].get_array('kpoints')[:, 1],
+        'kpoints_array2': results['kpoints'].get_array('kpoints')[:, 2],
+    })
+
+
+def test_opengrid_fftgrid(fixture_localhost, generate_calc_job_node, generate_parser):
+    """Test ``OpengridParser`` on the parsing of 'incompatible FFT grid' error."""
+    entry_point_calc_job = 'quantumespresso.opengrid'
+    entry_point_parser = 'quantumespresso.opengrid'
+
+    node = generate_calc_job_node(entry_point_calc_job, fixture_localhost, 'fftgrid')
+    parser = generate_parser(entry_point_parser)
+    _, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_failed, calcfunction.exit_status
+    assert calcfunction.exit_status == node.process_class.exit_codes.ERROR_INCOMPATIBLE_FFT_GRID.status
