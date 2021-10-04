e6e9080e1f758b85191727af2ab6a5c3d6f1aedb
Marnik Bercx <mbercx@gmail.com>
2021-10-04 18:33:52 +0200

    apply reviewer suggestions

--- aiida_quantumespresso/workflows/pdos.py
+++ aiida_quantumespresso/workflows/pdos.py
@@ -330,8 +330,7 @@ class PdosWorkChain(ProtocolMixin, WorkChain):
             sub processes that are called by this workchain.
         :return: a process builder instance with all inputs defined ready for launch.
         """
-
-
+        overrides = overrides or {}
         inputs = cls.get_protocol_inputs(protocol, overrides)
 
         args = (pw_code, structure, protocol)
@@ -340,9 +339,13 @@ class PdosWorkChain(ProtocolMixin, WorkChain):
         scf.pop('clean_workdir', None)
         nscf = PwBaseWorkChain.get_builder_from_protocol(*args, overrides=inputs.get('nscf', None), **kwargs)
         nscf['pw'].pop('structure', None)
-        nscf['pw']['parameters']['SYSTEM']['occupations'] = 'tetrahedra'
-        nscf['pw']['parameters']['SYSTEM'].pop('smearing', None)
-        nscf['pw']['parameters']['SYSTEM'].pop('degauss', None)
+        try:
+            occupations = overrides['nscf']['pw']['parameters']['SYSTEM']['occupations']
+            nscf['pw']['parameters']['SYSTEM']['occupations'] = occupations
+        except KeyError:
+            nscf['pw']['parameters']['SYSTEM']['occupations'] = 'tetrahedra'
+            nscf['pw']['parameters']['SYSTEM'].pop('smearing', None)
+            nscf['pw']['parameters']['SYSTEM'].pop('degauss', None)
         nscf.pop('clean_workdir', None)
 
         builder = cls.get_builder()
--- tests/workflows/protocols/test_pdos.py
+++ tests/workflows/protocols/test_pdos.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-"""Tests for the ``PwBandsWorkChain.get_builder_from_protocol`` method."""
+# pylint: disable=redefined-outer-name
+"""Tests for the ``PdosWorkChain.get_builder_from_protocol`` method."""
 import pytest
 
 from aiida.engine import ProcessBuilder
@@ -8,6 +9,17 @@ from aiida.plugins import WorkflowFactory
 from aiida_quantumespresso.common.types import ElectronicType, SpinType
 
 PdosWorkChain = WorkflowFactory('quantumespresso.pdos')
+
+
+@pytest.fixture
+def get_pdos_generator_inputs(fixture_code, generate_structure):
+    """Generate a set of default inputs for the ``PdosWorkChain.get_builder_from_protocol()`` method."""
+    return {
+        'pw_code': fixture_code('quantumespresso.pw'),
+        'dos_code': fixture_code('quantumespresso.dos'),
+        'projwfc_code': fixture_code('quantumespresso.projwfc'),
+        'structure': generate_structure('silicon')
+    }
 
 
 def test_get_available_protocols():
@@ -22,42 +34,23 @@ def test_get_default_protocol():
     assert PdosWorkChain.get_default_protocol() == 'moderate'
 
 
-def test_default(fixture_code, generate_structure, data_regression, serialize_builder):
+def test_default(get_pdos_generator_inputs, data_regression, serialize_builder):
     """Test ``PdosWorkChain.get_builder_from_protocol`` for the default protocol."""
-    pw_code = fixture_code('quantumespresso.pw')
-    dos_code = fixture_code('quantumespresso.dos')
-    projwfc_code = fixture_code('quantumespresso.projwfc')
-    structure = generate_structure()
-    builder = PdosWorkChain.get_builder_from_protocol(
-        pw_code=pw_code, dos_code=dos_code, projwfc_code=projwfc_code, structure=structure
-    )
+    inputs = get_pdos_generator_inputs
+    builder = PdosWorkChain.get_builder_from_protocol(**inputs)
 
     assert isinstance(builder, ProcessBuilder)
     data_regression.check(serialize_builder(builder))
 
 
-def test_electronic_type(fixture_code, generate_structure):
+def test_electronic_type(get_pdos_generator_inputs):
     """Test ``PdosWorkChain.get_builder_from_protocol`` with ``electronic_type`` keyword."""
-    pw_code = fixture_code('quantumespresso.pw')
-    dos_code = fixture_code('quantumespresso.dos')
-    projwfc_code = fixture_code('quantumespresso.projwfc')
-    structure = generate_structure()
-
     with pytest.raises(NotImplementedError):
         builder = PdosWorkChain.get_builder_from_protocol(
-            pw_code=pw_code,
-            dos_code=dos_code,
-            projwfc_code=projwfc_code,
-            structure=structure,
-            electronic_type=ElectronicType.AUTOMATIC
+            **get_pdos_generator_inputs, electronic_type=ElectronicType.AUTOMATIC
         )
-
     builder = PdosWorkChain.get_builder_from_protocol(
-        pw_code=pw_code,
-        dos_code=dos_code,
-        projwfc_code=projwfc_code,
-        structure=structure,
-        electronic_type=ElectronicType.INSULATOR
+        **get_pdos_generator_inputs, electronic_type=ElectronicType.INSULATOR
     )
     for namespace, occupations in zip((builder.scf, builder.nscf), ('fixed', 'tetrahedra')):
         parameters = namespace['pw']['parameters'].get_dict()
@@ -66,25 +59,12 @@ def test_electronic_type(fixture_code, generate_structure):
         assert 'smearing' not in parameters['SYSTEM']
 
 
-def test_spin_type(fixture_code, generate_structure):
+def test_spin_type(get_pdos_generator_inputs):
     """Test ``PdosWorkChain.get_builder_from_protocol`` with ``spin_type`` keyword."""
-    pw_code = fixture_code('quantumespresso.pw')
-    dos_code = fixture_code('quantumespresso.dos')
-    projwfc_code = fixture_code('quantumespresso.projwfc')
-    structure = generate_structure()
-
     with pytest.raises(NotImplementedError):
         for spin_type in [SpinType.NON_COLLINEAR, SpinType.SPIN_ORBIT]:
-            builder = PdosWorkChain.get_builder_from_protocol(
-                pw_code=pw_code, dos_code=dos_code, projwfc_code=projwfc_code, structure=structure, spin_type=spin_type
-            )
-    builder = PdosWorkChain.get_builder_from_protocol(
-        pw_code=pw_code,
-        dos_code=dos_code,
-        projwfc_code=projwfc_code,
-        structure=structure,
-        spin_type=SpinType.COLLINEAR
-    )
+            builder = PdosWorkChain.get_builder_from_protocol(**get_pdos_generator_inputs, spin_type=spin_type)
+    builder = PdosWorkChain.get_builder_from_protocol(**get_pdos_generator_inputs, spin_type=SpinType.COLLINEAR)
     for namespace in [builder.scf, builder.nscf]:
         parameters = namespace['pw']['parameters'].get_dict()
         assert parameters['SYSTEM']['nspin'] == 2
